b63c0731d840de3df17fa936641ca0bba7fdc5e8
PhuThanh001 <phunguyensc2001@gmail.com>
2025-07-22 16:44:22 +0700

    fix_underfineInRedux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './slides/counterSlide'; // Đường dẫn tương đối
+import userReducer from './slides/userSilde'; // Đường dẫn tương đối
 
 export const store = configureStore({
   reducer: {
     counter : counterReducer,
+    user: userReducer,
   },
+
 })
--- src/service/UserService.js
+++ src/service/UserService.js
@@ -10,4 +10,12 @@ export const registerUser = async (data) => {
     console.log('registerUser function called with data:', data);
     return res.data;
 }
-
+export const getUserDetails = async (id, token) => {
+    const res = await axios.get(`${import.meta.env.VITE_API_URL}/user/get-details/${id}`, {
+        headers: {
+            token: `Bearer ${token}`
+        }
+    });
+    console.log('getUserDetails function called with token:', token);
+    return res.data;
+}
